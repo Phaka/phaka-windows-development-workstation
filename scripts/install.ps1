fa18242d862611a8cd1e33087f5c9c807d63535b
Werner Strydom <bloudraak@users.noreply.github.com>
2018-01-05 14:21:20 -0800

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,37 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# Browsers 
+choco upgrade -y firefox 
+choco upgrade -y googlechrome 
+choco upgrade -y googlechrome.canary
+choco upgrade -y opera
+# choco upgrade -y chromium
+# choco upgrade -y vivaldi
+
 # Tools
 choco upgrade -y nmap
+choco upgrade -y sysinternals
+choco upgrade -y 7zip
+choco upgrade -y winimage
+# choco upgrade -y dropbox
+# choco upgrade -y onedrive
+# choco upgrade -y googledrive
+
+# WebEx
+choco upgrade -y webexplayer
+
+# SSH Tools
+# choco upgrade -y winscp
+choco upgrade -y bitvise-ssh-client
+# choco upgrade -y bitvise-ssh-server
+# choco upgrade -y openssh
+
+# Adobe
+choco upgrade -y adobereader
+choco upgrade -y flashplayerplugin
+choco upgrade -y adobeair
+# choco upgrade -y adobeshockwaveplayer
+# choco upgrade -y adobe-creative-cloud
 
 # Git
 choco upgrade -y git
@@ -13,29 +43,70 @@ choco upgrade -y gitkraken
 # Web Platform Installer
 choco upgrade -y webpi
 
-# JDK
-choco upgrade -y jre7 
-choco upgrade -y jre8 
-choco upgrade -y jre9 
+# Docker
+choco upgrade -y docker
 
-# Browsers 
-choco upgrade -y firefox 
-choco upgrade -y googlechrome 
-choco upgrade -y googlechrome.canary
-choco upgrade -y opera
+# Go Language
+choco upgrade -y golang
+
+# JDK
+choco upgrade -y jdk7 
+choco upgrade -y jdk8 
+choco upgrade -y jdk9 
+
+# Eclipse
+# choco install eclipse
+
+
 
 # Python
 choco upgrade -y python2
 choco upgrade -y python3
 
+# VMware
+choco upgrade -y vmwareworkstation
+# choco upgrade -y vmwareplayer
+
+# Vagrant 
+choco upgrade -y vagrant
+
+# Packer
+choco upgrade -y packer
+choco upgrade -y packer-post-processor-vagrant-vmware-ovf
+
 # Ruby
+# choco upgrade -y pik
 choco upgrade -y ruby
 choco upgrade -y ruby2.devkit
 
 # NodeJS
 choco upgrade -y nodejs
 
-# .NET Framework
+# C++ Development
+choco upgrade -y cmake
+
+# Protocol Buffers
+choco upgrade -y protoc
+
+# .NET Core SDKs
+choco upgrade -y dotnetcore-sdk --version 1.0
+choco upgrade -y dotnetcore-sdk --version 1.1
+choco upgrade -y dotnetcore-sdk --version 2.0
+choco upgrade -y dotnetcore-sdk --version 2.1
+
+# .NET Framework 
+choco upgrade -y dotnet3.5
+choco upgrade -y dotnet4.5
+choco upgrade -y dotnet4.5.1
+choco upgrade -y dotnet4.5.2
+choco upgrade -y dotnet4.6.1
+choco upgrade -y dotnet-4.6.2
+choco upgrade -y dotnet4.6
+choco upgrade -y dotnet4.6.2
+choco upgrade -y dotnet4.7
+choco upgrade -y dotnet4.7.1
+
+# .NET Framework Development Packs
 choco upgrade -y netfx-4.0.3-devpack
 choco upgrade -y netfx-4.5.2-devpack
 choco upgrade -y netfx-4.6-devpack
@@ -44,7 +115,12 @@ choco upgrade -y netfx-4.6.2-devpack
 choco upgrade -y netfx-4.7-devpack
 choco upgrade -y netfx-4.7.1-devpack
 
+# Microsoft .NET Framework Repair Tool (https://support.microsoft.com/en-us/help/2698555/microsoft--net-framework-repair-tool-is-available)
+choco upgrade -y dotnet-verification-tool
+
 # Windows SDKs
+choco upgrade -y windows-sdk-6.0
+choco upgrade -y windows-sdk-6.1
 choco upgrade -y windows-sdk-7.0
 choco upgrade -y windows-sdk-7.1
 choco upgrade -y windows-sdk-8.0
@@ -52,8 +128,12 @@ choco upgrade -y windows-sdk-8.1
 choco upgrade -y windows-sdk-10.0
 choco upgrade -y windows-sdk-10.1
 
-# Atom Editor
+# Editors
 choco upgrade -y atom
+choco upgrade -y notepadplusplus
+
+# NuGet
+choco upgrade -y nuget.commandline
 
 # Visual Studio Code
 choco upgrade -y visualstudiocode
@@ -83,11 +163,30 @@ choco upgrade -y visualstudio2015professional
 choco upgrade -y visualstudio2017professional
 # choco upgrade -y visualstudio2017enterprise
 
+# Java Development
+choco upgrade -y gradle
 
 # JetBrains
 choco upgrade -y jetbrainstoolbox
-choco upgrade -y resharper-cltchoco install clion-ide
-
+choco upgrade -y resharper-clt
+choco upgrade -y clion-ide
+choco upgrade -y phpstorm
+choco upgrade -y datagrip
+choco upgrade -y webstorm
+choco upgrade -y pycharm
+choco upgrade -y kotlinc
+# choco upgrade -y pycharm-community
+choco upgrade -y intellijidea-ultimate
+# choco upgrade -y intellijidea-community
+choco upgrade -y resharper-ultimate-all
+# choco upgrade -y resharper
+# choco upgrade -y dotpeek
+# choco upgrade -y dotcover
+# choco upgrade -y dotcover-cli
+# choco upgrade -y dottrace
+# choco upgrade -y dotmemory
+# choco upgrade -y resharpercpp
+# choco upgrade -y teamcityaddin
 
 # WiX Toolset
 choco upgrade -y wixtoolset
@@ -104,4 +203,48 @@ choco upgrade -y rdm
 # choco upgrade -y rdmfree 
 # choco upgrade -y rdmagent
 
+# Slack
+choco upgrade -y slack
+# choco upgrade -y microsoft-teams
+choco upgrade -y gitter
+
+# SQL Server
+# choco upgrade -y sql-server-management-studio
+# choco upgrade -y sqlserver-odbcdriver
+# choco upgrade -y sqlserverlocaldb
+# choco upgrade -y sqljdbc
+
+# PostgreSql
+# choco upgrade -y postgresql
+
+# MySQL
+# choco upgrade -y mysql
+# choco upgrade -y mysql.workbench
+
+# Kindle
+choco upgrade -y kindle
+choco upgrade -y sendtokindle
+choco upgrade -y kindlegen
+
+# Apache
+choco upgrade -y maven
+choco upgrade -y ant
+choco upgrade -y svn
+# choco upgrade -y jmeter
+# choco upgrade -y activemq
+
+# Tools
+choco upgrade -y linqpad
+choco upgrade -y azcopy
+choco upgrade -y microsoftsecurityessentials
+choco upgrade -y mbca # Microsoft Baseline Configuration Analyze
+
+# Cygwin
+# choco upgrade -y cygwin
+
+# msys2
+choco upgrade -y msys2
+
+# mingw
+# choco upgrade -y mingw
 
