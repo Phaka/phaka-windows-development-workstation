8aedb9ff65749ad90d124321080a3756f97d45df
Werner Strydom <bloudraak@users.noreply.github.com>
2018-01-06 13:47:34 -0800

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -92,7 +92,7 @@ configuration DevelopmentMachine
         #------------------------------------------------------------------------------------------------------------------
         # .NET Framework Versions
         #------------------------------------------------------------------------------------------------------------------
-        cChocoPackageInstaller Netfx35
+        cChocoPackageInstaller dotnet35
         {
             Name = 'dotnet3.5'
             Ensure = 'Present'
@@ -155,6 +155,117 @@ configuration DevelopmentMachine
             DependsOn = '[cChocoInstaller]Chocolatey'
             AutoUpgrade = $True
         }          
+        
+        #------------------------------------------------------------------------------------------------------------------
+        # .NET Framework Development Packs
+        #------------------------------------------------------------------------------------------------------------------
+        cChocoPackageInstaller NetFx452devpack
+        {
+            Name = 'netfx-4.5.2-devpack'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]dotnet452'
+            AutoUpgrade = $True
+        } 
+        
+        cChocoPackageInstaller Netfx46devpack
+        {
+            Name = 'netfx-4.6-devpack'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]dotnet46'
+            AutoUpgrade = $True
+        } 
+        
+        cChocoPackageInstaller Netfx461devpack
+        {
+            Name = 'netfx-4.6.1-devpack'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]dotnet461'
+            AutoUpgrade = $True
+        } 
+        
+        cChocoPackageInstaller Netfx462devpack
+        {
+            Name = 'netfx-4.6.2-devpack'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]dotnet462'
+            AutoUpgrade = $True
+        } 
+        
+        cChocoPackageInstaller Netfx47devpack
+        {
+            Name = 'netfx-4.7-devpack'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]dotnet47'
+            AutoUpgrade = $True
+        } 
+        
+        cChocoPackageInstaller Netfx471devpack
+        {
+            Name = 'netfx-4.7.1-devpack'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]dotnet471'
+            AutoUpgrade = $True
+        } 
+
+        #------------------------------------------------------------------------------------------------------------------
+        # Windows SDKs
+        #------------------------------------------------------------------------------------------------------------------
+        cChocoPackageInstaller WindowsSDK60
+        {
+            Name = 'windows-sdk-6.0'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }
+        cChocoPackageInstaller WindowsSDK61
+        {
+            Name = 'windows-sdk-6.1'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }      
+        cChocoPackageInstaller WindowsSDK70
+        {
+            Name = 'windows-sdk-7.0'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }         
+        cChocoPackageInstaller WindowsSDK71
+        {
+            Name = 'windows-sdk-7.1'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }  
+        cChocoPackageInstaller WindowsSDK80
+        {
+            Name = 'windows-sdk-8.0'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }         
+        cChocoPackageInstaller WindowsSDK81
+        {
+            Name = 'windows-sdk-8.1'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }  
+        cChocoPackageInstaller WindowsSDK10
+        {
+            Name = 'windows-sdk-10.0'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        }  
+        cChocoPackageInstaller WindowsSDK101
+        {
+            Name = 'windows-sdk-10.1'
+            Ensure = 'Present'
+            DependsOn = '[cChocoInstaller]Chocolatey'
+            AutoUpgrade = $True
+        } 
     }
 }
 
