f54f858b27e517a3f2df660f30e1eb7adb7d054f
Werner Strydom <bloudraak@users.noreply.github.com>
2018-01-06 12:53:21 -0800

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -10,6 +10,6 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 #--------------------------------------------------------------------------
 choco upgrade -y boxstarter
 
+Import-Module Boxstarter.Chocolatey
 
-
-
+# Install-BoxstarterPackage -PackageName https://gist.github.com/mwrock/8066325/raw/e0c830528429cd68a8c71dbff6f48298576d8d20/gistfile1.txt
