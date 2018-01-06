c35cb958f185d8e855c74faa4db434c36d594511
Werner Strydom <werners@bloudraak.com>
2018-01-06 11:54:40 -0800

    Update Install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,48 +1,60 @@
 #Requires -Version 5
 
+#--------------------------------------------------------------------------
+# Chocolatey
+#--------------------------------------------------------------------------
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# PowerShell
-choco upgrade -y dotnet4.5.2
-choco upgrade -y powershell
-# TODO: Only install if PowerShell 5.1 is installed
-# choco upgrade -y powershell-packagemanagement
-
+#--------------------------------------------------------------------------
+# Windows Features
+#--------------------------------------------------------------------------
 # choco upgrade -y IIS-WebServerRole --source windowsfeatures
 
-# Background Information
-choco upgrade -y bginfo
+#--------------------------------------------------------------------------
+# PowerShell
+#--------------------------------------------------------------------------
+choco upgrade -y dotnet4.5.2 powershell powershell-packagemanagement
 
+#--------------------------------------------------------------------------
+# Browsers
+#--------------------------------------------------------------------------
+# Background Information
+# choco upgrade -y bginfo
+
+#--------------------------------------------------------------------------
+# Browsers
+#--------------------------------------------------------------------------
 # Browsers 
-choco upgrade -y firefox 
-choco upgrade -y googlechrome 
-choco upgrade -y googlechrome.canary
-choco upgrade -y opera
-# choco upgrade -y chromium
-# choco upgrade -y vivaldi
+choco upgrade -y firefox `
+    googlechrome `
+    googlechrome.canary `
+    # chromium `
+    # vivaldi `
+    opera 
+
+Stop-Process -Name "Opera"  
+Stop-Process -Name "Chrome"  
+Stop-Process -Name "Firefox"  
 
 # Fonts
-choco upgrade -y ubuntu.font
-choco upgrade -y sourcecodepro
-# choco upgrade -y droidsansmono
-# choco upgrade -y droidfonts
-choco upgrade -y dejavufonts
-choco upgrade -y anonymouspro
-choco upgrade -y bitstreamverafonts
-# choco upgrade -y firacode
-choco upgrade -y hackfont
-choco upgrade -y robotofonts
+# choco upgrade -y ubuntu.font
+# choco upgrade -y sourcecodepro
+# choco upgrade -y dejavufonts
+# choco upgrade -y anonymouspro
+# choco upgrade -y bitstreamverafonts
+# choco upgrade -y hackfont
+#choco upgrade -y robotofonts
 # choco upgrade -y noto
 # choco upgrade -y oxygenmono
 # choco upgrade -y opensans
 # choco upgrade -y font-awesome-font
 
 # Tools
-choco upgrade -y nmap
-choco upgrade -y sysinternals
-choco upgrade -y 7zip
-choco upgrade -y winimage
-choco upgrade -y grammarly
+# choco upgrade -y nmap
+# choco upgrade -y sysinternals
+# choco upgrade -y 7zip
+# choco upgrade -y winimage
+# choco upgrade -y grammarly
 
 # File Sharing
 # choco upgrade -y dropbox
@@ -54,35 +66,35 @@ choco upgrade -y grammarly
 
 # SSH Tools
 # choco upgrade -y winscp
-choco upgrade -y bitvise-ssh-client
+# choco upgrade -y bitvise-ssh-client
 # choco upgrade -y bitvise-ssh-server
 # choco upgrade -y openssh
 
 # Adobe
-choco upgrade -y adobereader
-choco upgrade -y flashplayerplugin
-choco upgrade -y adobeair
+# choco upgrade -y adobereader
+# choco upgrade -y flashplayerplugin
+# choco upgrade -y adobeair
 # choco upgrade -y adobeshockwaveplayer
 # choco upgrade -y adobe-creative-cloud
 
 # Comparing Documents
-choco upgrade -y winmerge
+# choco upgrade -y winmerge
 
 # Git
-choco upgrade -y git
-choco upgrade -y git-lfs
+# choco upgrade -y git
+# choco upgrade -y git-lfs
 
 # Mercurial
-choco upgrade -y hg
+# choco upgrade -y hg
 
 # SVN
-choco upgrade -y svn
+# choco upgrade -y svn
 
 # Bazaar
-# bzr
+# choco upgrade -y bzr
 
 # GitKraken
-choco upgrade -y gitkraken
+# choco upgrade -y gitkraken
 # choco upgrade -y smartgit
 # choco upgrade -y sourcetree
 # choco upgrade -y tortoisehg
@@ -90,110 +102,109 @@ choco upgrade -y gitkraken
 # choco upgrade -y tortoisegit
 
 # Web Platform Installer
-choco upgrade -y webpi
+# choco upgrade -y webpi
 
 # Docker
-choco upgrade -y docker
+# choco upgrade -y docker
 
 # Go Language
-choco upgrade -y golang
+# choco upgrade -y golang
 
 # Haskel
-choco upgrade -y haskell-stack
+# choco upgrade -y haskell-stack
 
 # JDK
-choco upgrade -y jdk8 
-choco upgrade -y jdk9 
+# choco upgrade -y jdk8 
+# choco upgrade -y jdk9 
 
 # Eclipse
 # choco upgrade -y eclipse
 
 # Python
-choco upgrade -y python2
-choco upgrade -y python
+# choco upgrade -y python2
+# choco upgrade -y python
 
 # Clojure 
 # choco upgrade -y clojure
 
 # VMware
-choco upgrade -y vmwareworkstation --version 12.5.7
+# choco upgrade -y vmwareworkstation --version 12.5.7
 # choco upgrade -y vmwareplayer
 
 # Vagrant 
-choco upgrade -y vagrant
+# choco upgrade -y vagrant
 
 # Packer
-choco upgrade -y packer
-choco upgrade -y packer-post-processor-vagrant-vmware-ovf
+# choco upgrade -y packer
+# choco upgrade -y packer-post-processor-vagrant-vmware-ovf
 
 # Ruby
 # choco upgrade -y pik
-choco upgrade -y ruby
-choco upgrade -y ruby2.devkit
+# choco upgrade -y ruby
+# choco upgrade -y ruby2.devkit
 
 # NodeJS
-choco upgrade -y nodejs
-choco upgrade -y nodejs-lts
-choco upgrade -y yarn
-choco upgrade -y bower
-choco upgrade -y gulp-cli
+# choco upgrade -y nodejs
+# choco upgrade -y nodejs-lts
+# choco upgrade -y yarn
+# choco upgrade -y bower
+# choco upgrade -y gulp-cli
 
 # Protocol Buffers
-choco upgrade -y protoc
+# choco upgrade -y protoc
 
 # .NET Core SDKs
-choco upgrade -y dotnetcore-sdk --version 1.0
-choco upgrade -y dotnetcore-sdk --version 1.1
-choco upgrade -y dotnetcore-sdk --version 2.0
-choco upgrade -y dotnetcore-sdk --version 2.1
+# choco upgrade -y dotnetcore-sdk --version 1.0
+# choco upgrade -y dotnetcore-sdk --version 1.1
+# choco upgrade -y dotnetcore-sdk --version 2.0
+# choco upgrade -y dotnetcore-sdk --version 2.1
 
 # .NET Framework 
-choco upgrade -y dotnet3.5
-choco upgrade -y dotnet4.5
-choco upgrade -y dotnet4.5.1
-
-choco upgrade -y dotnet4.6.1
-choco upgrade -y dotnet-4.6.2
-choco upgrade -y dotnet4.6
-choco upgrade -y dotnet4.6.2
-choco upgrade -y dotnet4.7
-choco upgrade -y dotnet4.7.1
+# choco upgrade -y dotnet3.5
+# choco upgrade -y dotnet4.5
+# choco upgrade -y dotnet4.5.1
+# choco upgrade -y dotnet4.5.1
+# choco upgrade -y dotnet4.6
+# choco upgrade -y dotnet4.6.1
+# choco upgrade -y dotnet4.6.2
+# choco upgrade -y dotnet4.7
+# choco upgrade -y dotnet4.7.1
 
 # .NET Framework Development Packs
-choco upgrade -y netfx-4.0.3-devpack
-choco upgrade -y netfx-4.5.2-devpack
-choco upgrade -y netfx-4.6-devpack
-choco upgrade -y netfx-4.6.1-devpack
-choco upgrade -y netfx-4.6.2-devpack
-choco upgrade -y netfx-4.7-devpack
-choco upgrade -y netfx-4.7.1-devpack
+# choco upgrade -y netfx-4.0.3-devpack
+# choco upgrade -y netfx-4.5.2-devpack
+# choco upgrade -y netfx-4.6-devpack
+# choco upgrade -y netfx-4.6.1-devpack
+# choco upgrade -y netfx-4.6.2-devpack
+# choco upgrade -y netfx-4.7-devpack
+# choco upgrade -y netfx-4.7.1-devpack
 
 # Microsoft .NET Framework Repair Tool (https://support.microsoft.com/en-us/help/2698555/microsoft--net-framework-repair-tool-is-available)
-choco upgrade -y dotnet-verification-tool
+# choco upgrade -y dotnet-verification-tool
 
 # Windows SDKs
-choco upgrade -y windows-sdk-6.0
-choco upgrade -y windows-sdk-6.1
-choco upgrade -y windows-sdk-7.0
-choco upgrade -y windows-sdk-7.1
-choco upgrade -y windows-sdk-8.0
-choco upgrade -y windows-sdk-8.1
-choco upgrade -y windows-sdk-10.0
-choco upgrade -y windows-sdk-10.1
+# choco upgrade -y windows-sdk-6.0
+# choco upgrade -y windows-sdk-6.1
+# choco upgrade -y windows-sdk-7.0
+# choco upgrade -y windows-sdk-7.1
+# choco upgrade -y windows-sdk-8.0
+# choco upgrade -y windows-sdk-8.1
+# choco upgrade -y windows-sdk-10.0
+# choco upgrade -y windows-sdk-10.1
 
 # Editors
-choco upgrade -y atom
-choco upgrade -y notepadplusplus
-choco upgrade -y sublimetext3
+# choco upgrade -y atom
+# choco upgrade -y notepadplusplus
+# choco upgrade -y sublimetext3
 
 # Mono
-choco upgrade -y mono
-choco upgrade -y gtksharp
-choco upgrade -y monodevelop
+# choco upgrade -y mono
+# choco upgrade -y gtksharp
+# choco upgrade -y monodevelop
 
 # NuGet
-choco upgrade -y nuget.commandline
-choco upgrade -y nugetpackageexplorer
+# choco upgrade -y nuget.commandline
+# choco upgrade -y nugetpackageexplorer
 
 # Octopus Deploy
 # choco upgrade -y octopustools
@@ -201,15 +212,15 @@ choco upgrade -y nugetpackageexplorer
 # CMake
 
 # Visual Studio Code
-choco upgrade -y visualstudiocode
-choco upgrade -y vscode-powershell
-choco upgrade -y vscode-csharp
-choco upgrade -y vscode-docker
-choco upgrade -y vscode-cake
-choco upgrade -y vscode-gitattributes
-choco upgrade -y vscode-gitlens
+# choco upgrade -y visualstudiocode
+# choco upgrade -y vscode-powershell
+# choco upgrade -y vscode-csharp
+# choco upgrade -y vscode-docker
+# choco upgrade -y vscode-cake
+# choco upgrade -y vscode-gitattributes
+# choco upgrade -y vscode-gitlens
 
-choco upgrade -y cmake
+# choco upgrade -y cmake
 
 # Visual Studio 2012
 # choco upgrade -y visualstudio2012ultimate
@@ -219,24 +230,24 @@ choco upgrade -y cmake
 # choco upgrade -y visualstudio2013premium
 
 # Visual Studio 2015
-choco upgrade -y visualstudio2015professional
+# choco upgrade -y visualstudio2015professional
 # choco upgrade -y visualstudio2015enterprise
 # choco upgrade -y ssdt15
 # choco upgrade -y visualstudio2015-powershelltools
 # choco upgrade -y poshtools-visualstudio2015
 
 # Visual Studio 2017
-choco upgrade -y visualstudio2017professional
+# choco upgrade -y visualstudio2017professional
 # choco upgrade -y visualstudio2017enterprise
 
 # GhostDoc Enterprise
-choco upgrade -y ghostdoc-enterprise
+# choco upgrade -y ghostdoc-enterprise
 
 # Java Development
-choco upgrade -y gradle
-choco upgrade -y maven
-choco upgrade -y ant
-choco upgrade -y ivy
+# choco upgrade -y gradle
+# choco upgrade -y maven
+# choco upgrade -y ant
+# choco upgrade -y ivy
 
 # Spring Tool Suite
 # choco upgrade -y springtoolsuite
@@ -245,30 +256,30 @@ choco upgrade -y ivy
 # choco upgrade -y netbeans
 
 # C++
-choco upgrade -y cmake
-choco upgrade -y conan
-choco upgrade -y doxygen.install
+# choco upgrade -y cmake
+# choco upgrade -y conan
+# choco upgrade -y doxygen.install
 
 # PHP Development
-choco upgrade -y php
-choco upgrade -y composer
+# choco upgrade -y php
+# choco upgrade -y composer
 
 # C# Development
-choco upgrade -y nswagstudio
+# choco upgrade -y nswagstudio
 
 # JetBrains
-choco upgrade -y jetbrainstoolbox
-choco upgrade -y resharper-clt
-choco upgrade -y clion-ide
-choco upgrade -y phpstorm
-choco upgrade -y datagrip
-choco upgrade -y webstorm
-choco upgrade -y pycharm
-choco upgrade -y kotlinc
+# choco upgrade -y jetbrainstoolbox
+# choco upgrade -y resharper-clt
+# choco upgrade -y clion-ide
+# choco upgrade -y phpstorm
+# choco upgrade -y datagrip
+# choco upgrade -y webstorm
+# choco upgrade -y pycharm
+# choco upgrade -y kotlinc
 # choco upgrade -y pycharm-community
-choco upgrade -y intellijidea-ultimate
+# choco upgrade -y intellijidea-ultimate
 # choco upgrade -y intellijidea-community
-choco upgrade -y resharper-ultimate-all
+# choco upgrade -y resharper-ultimate-all
 # choco upgrade -y resharper
 # choco upgrade -y dotpeek
 # choco upgrade -y dotcover
@@ -279,32 +290,32 @@ choco upgrade -y resharper-ultimate-all
 # choco upgrade -y teamcityaddin
 
 # SpecFlow
-choco upgrade -y specflow
+# choco upgrade -y specflow
 
 # WiX Toolset
-choco upgrade -y wixtoolset
+# choco upgrade -y wixtoolset
 
 # WebDeploy
-choco upgrade -y webdeploy
+# choco upgrade -y webdeploy
 
 # PowerShell
-choco upgrade -y pester
-choco upgrade -y pscx # PowerShell Community Extensions 
-choco upgrade -y psake
-choco upgrade -y carbon
+# choco upgrade -y pester
+# choco upgrade -y pscx # PowerShell Community Extensions 
+# choco upgrade -y psake
+# choco upgrade -y carbon
 
 # AWS
-choco upgrade -y awstools.powershell
+# choco upgrade -y awstools.powershell
 
 # Packet
-choco upgrade -y paket
+# choco upgrade -y paket
 
 # 1Password
-choco upgrade -y 1password
-choco upgrade -y op # 1Password CLI
+# choco upgrade -y 1password
+# choco upgrade -y op # 1Password CLI
 
 # Remote Desktop Manager
-choco upgrade -y rdm 
+# choco upgrade -y rdm 
 # choco upgrade -y rdmfree 
 # choco upgrade -y rdmagent
 
@@ -312,9 +323,9 @@ choco upgrade -y rdm
 # choco upgrade -y royalts
 
 # Slack
-choco upgrade -y slack
+# choco upgrade -y slack
 # choco upgrade -y microsoft-teams
-choco upgrade -y gitter
+# choco upgrade -y gitter
 
 # SQL Server
 # choco upgrade -y sql-server-express
@@ -337,22 +348,22 @@ choco upgrade -y gitter
 # choco upgrade -y mysql.workbench
 
 # Kindle
-choco upgrade -y kindle
-choco upgrade -y sendtokindle
-choco upgrade -y kindlegen
+# choco upgrade -y kindle
+# choco upgrade -y sendtokindle
+# choco upgrade -y kindlegen
 
 # Apache
-choco upgrade -y maven
-choco upgrade -y ant
-choco upgrade -y svn
+# choco upgrade -y maven
+# choco upgrade -y ant
+# choco upgrade -y svn
 # choco upgrade -y jmeter
 # choco upgrade -y activemq
 
 # Tools
-choco upgrade -y linqpad
-choco upgrade -y azcopy
-choco upgrade -y microsoftsecurityessentials
-choco upgrade -y mbca # Microsoft Baseline Configuration Analyze
+# choco upgrade -y linqpad
+# choco upgrade -y azcopy
+# choco upgrade -y microsoftsecurityessentials
+# choco upgrade -y mbca # Microsoft Baseline Configuration Analyze
 
 # GNU Privacy Guard for Windows
 # choco upgrade -y gpg4win-vanilla
@@ -363,20 +374,20 @@ choco upgrade -y mbca # Microsoft Baseline Configuration Analyze
 # choco upgrade -y cygwin
 
 # msys2
-choco upgrade -y msys2
+# choco upgrade -y msys2
 
 # mingw
 # choco upgrade -y mingw
 
 # Azure
 # choco upgrade -y azurepowershell --version 0.9.3
-choco upgrade -y windowsazurepowershell
-choco upgrade -y azure-cli
-choco upgrade -y zurestorageexplorer
-choco upgrade -y servicebusexplorer
-choco upgrade -y azuredatafactorytools15
-choco upgrade -y azure-documentdb-emulator
-choco upgrade -y vscode-azurerm-tools
+# choco upgrade -y windowsazurepowershell
+# choco upgrade -y azure-cli
+# choco upgrade -y zurestorageexplorer
+# choco upgrade -y servicebusexplorer
+# choco upgrade -y azuredatafactorytools15
+# choco upgrade -y azure-documentdb-emulator
+# choco upgrade -y vscode-azurerm-tools
 
 # Antlr
 # choco upgrade -y antlrworks2
@@ -396,27 +407,27 @@ choco upgrade -y vscode-azurerm-tools
 # choco upgrade -y picklesui
 
 # Microsoft Office
-choco upgrade -y onenote
+# choco upgrade -y onenote
 
 # R
 # choco upgrade -y r.studio
 # choco upgrade -y r.project
 
 # Log Parser Studio
-choco upgrade -y logparserstudio
+# choco upgrade -y logparserstudio
 
 # Snagit
-choco upgrade -y snagit --version 13.1.1
+# choco upgrade -y snagit --version 13.1.1
 
 # Networking
-choco upgrade -y winpcap
-choco upgrade -y wireshark
-choco upgrade -y microsoft-message-analyzer
+# choco upgrade -y winpcap
+# choco upgrade -y wireshark
+# choco upgrade -y microsoft-message-analyzer
 
 # yubico
-choco upgrade -y yubico-authenticator
-choco upgrade -y yubikey-neo-manager
-choco upgrade -y yubikey-personalization-tool
+# choco upgrade -y yubico-authenticator
+# choco upgrade -y yubikey-neo-manager
+# choco upgrade -y yubikey-personalization-tool
 
 # Objective-C for Windows
 # choco upgrade -y upgrade winobjc-tools
@@ -427,11 +438,11 @@ choco upgrade -y yubikey-personalization-tool
 # choco upgrade -y picassio
 
 # Puppet
-choco upgrade -y puppet
-choco upgrade -y geppetto
+# choco upgrade -y puppet
+# choco upgrade -y geppetto
 
 # Arduino IDE
-choco upgrade -y arduino
+# choco upgrade -y arduino
 
 
 
