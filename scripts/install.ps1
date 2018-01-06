5960e226ffe58b2d157cb36a452e4034581d79c0
Werner Strydom <bloudraak@users.noreply.github.com>
2018-01-06 13:00:54 -0800

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -3,11 +3,16 @@
 #Main-function
 function main {
     $rc = Update-Chocolatey
+    Test-Reboot
 }
 
 #Helpers
 function Update-Chocolatey {
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1?t="+ DateTime.Now.ToLocalTime()'))
+}
+
+function Test-Reboot {
+    $False
 }
 
 #Entry point
