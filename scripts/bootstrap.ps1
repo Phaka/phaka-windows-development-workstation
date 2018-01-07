9d16b4c8f5683227d6635cbb907ddf2c497832d5
Werner Strydom <bloudraak@users.noreply.github.com>
2018-01-07 11:19:54 -0800

    Update bootstrap.ps1

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,6 +1,9 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+choco install -y BoxStarter
+
+# Configure WinRM for the DSC Script
 winrm quickconfig -quiet
 Set-Item -Path WSMan:\localhost\MaxEnvelopeSizeKb -Value 8192
 
+# DSC Modules
 Install-Module -Name cChoco -Force
-Install-Module -Name xNetworking -Force
