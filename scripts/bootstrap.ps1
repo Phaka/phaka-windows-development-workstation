4f132e942bec63a41a2ecb4067851f8cbe65f47b
Werner Strydom <bloudraak@users.noreply.github.com>
2018-01-07 16:41:03 -0800

    Update bootstrap.ps1

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -6,8 +6,8 @@ winrm quickconfig -quiet
 Set-Item -Path WSMan:\localhost\MaxEnvelopeSizeKb -Value 8192
 
 # DSC Modules
-Install-PackageProvider Nuget –force –verbose
-Install-PackageProvider chocolatey –force –verbose
+Install-PackageProvider Nuget -force -verbose
+Install-PackageProvider chocolatey -force -verbose
 Install-Module -Name cChoco -Force
 
 # Get the party started
